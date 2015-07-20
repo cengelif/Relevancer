499601c3fdecb3b59c13737819750d64e00b7b2b
floodtags <boortman@floodtags.com>
2015-07-20 15:03:07 +0200

    Changed size

--- relevancerdb.py
+++ relevancerdb.py
@@ -13,7 +13,7 @@ from bson.objectid import ObjectId
 # parser.add_argument('-c', '--collection', type=str, required=True, help='collection name of the tweets')
 # args = parser.parse_args()
 
-collection = 'healthtags_big'
+collection = 'indonesia_big'
 
 rlvdb, rlvcl = rlv.connect_mongodb(configfile='data/mongodb.ini', coll_name=collection)
 
@@ -23,7 +23,7 @@ begin = ObjectId('54a48e078a8d5e2e4016a796')
 end = ObjectId('5584043ba023cf5c336ba0cd')
 # tweetlist = rlv.read_json_tweets_database(rlvcl, mongo_query={'_id': {'$gte': begin, '$lte': end}}, tweet_count=3000, reqlang='en')
 # tweetlist = rlv.read_json_tweets_database(rlvcl, mongo_query={'_id': {'$gte': begin}}, tweet_count=50000, reqlang='in')
-tweetlist = rlv.read_json_tweets_database(rlvcl, mongo_query={}, tweet_count=500000, reqlang='in')
+tweetlist = rlv.read_json_tweets_database(rlvcl, mongo_query={}, tweet_count=5000000, reqlang='in')
 rlv.logging.info("number of tweets"+str(len(tweetlist)))
 #print(len(tweetlist))	
 tweetsDF = rlv.create_dataframe(tweetlist)

9def785edfc63e660544feeabdcfce8a38b117fb
cengelif <eturkay35@gmail.com>
2015-07-23 15:04:35 +0200

    adding jaccard and entropy

--- relevancerdb.py
+++ relevancerdb.py
@@ -46,6 +46,8 @@ rlv.logging.info("This text overwritten by tokenizer" + str(tstDF["text"]))
 print("normalization:",tstDF["active_text"])#.iloc[10])
 rlv.logging.info("This text overwritten by normalization" + str(tstDF["active_text"]))
 
+find_distance = rlv.get_vectorizer_and_distance(tstDF)
+
 cluster_list = rlv.create_clusters(tweetsDF, my_token_pattern, nameprefix='1-') # those comply to selection criteria
 #cluster_list2 = rlv.create_clusters(tweetsDF, selection=False) # get all clusters. You can consider it at the end.
 print (len(cluster_list))  
@@ -63,7 +65,7 @@ print("cluster_prefix", a_cluster['cnoprefix'] )
 
 print("cluster_tuple_list", a_cluster['ctweettuplelist'] )
 
-print("cluster_entropy", a_cluster['user_ent'] )
+print("cluster_entropy", a_cluster['user_entropy'] )
 
 collection_name = collection + '_clusters'
 rlvdb[collection_name].insert(cluster_list) #Each iteration results with a candidate cluster list. Each iteration will have its own list. Therefore they are not mixed.
